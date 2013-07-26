842ea3875f97d6e365e7b27b5d0280339bbb6393
Matt Parker <mattmparker@gmail.com>
2013-07-26 17:10:06 -0600

    Improve package doc and imports.

--- R/tbdbaid-package.r
+++ R/tbdbaid-package.r
@@ -1,5 +1,13 @@
-#' tbdbaid
+#' tbdbaid: a collection of functions for querying TBdb.
 #'
-#' @name tbdbaid
+#' This package provides a collection of functions for making standardized
+#' queries against TBdb, TBdbPlus, and occassionally the Denver Health
+#' Data Warehouse.
+#'
+#' @name tbdbaid-package
+#' @aliases tbdbaid
 #' @docType package
+#' @author Matt Parker \email{mattmparker@@gmail.com}
+#' @keywords package tbdb
+#' @import RODBC reshape2
 NULL
