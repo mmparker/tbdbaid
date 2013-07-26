54b3b1811db6e0724736c40c674e6bf1840dba74
Matt Parker <mattmparker@gmail.com>
2013-07-26 16:45:50 -0600

    Set up a connect_to_ analog for the DHDW.

---
+++ R/connect_to_dhdw.r
@@ -0,0 +1,27 @@
+
+
+
+
+#' Connect to the right DH Data Warehouse ODBC drivers, regardless of which
+#' version of R is running
+#'
+#' This functions connects to the correct set of ODBC drivers for the DHDW,
+#' regardless of which architecture R is running on.
+#'
+#' @keywords database, odbc
+#' @export
+#' 
+#' @examples
+#' plus <- connect_to_tbdbplus()
+
+connect_to_dhdw <- function() {
+
+    arch <- R.Version()$arch
+
+    if(arch %in% "x86_64") { 
+        return(odbcConnect("dhdw64"))
+    } else {
+        return(odbcConnect("dhdw32"))
+    }
+
+}
